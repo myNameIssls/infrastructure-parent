6e3d668f6ec01c79afd9f0c6073ad02d41b205d7
尚立帅 <shanglishuai@iciyun.com>
2024-01-16 18:01:30 +0800

    spring-authorization-server-sample

---
+++ infrastructure-core/src/main/java/com/tyrone/infrastructure/core/pl/Response.java
@@ -0,0 +1,11 @@
+package com.tyrone.infrastructure.core.pl;
+
+import java.io.Serializable;
+
+public class Response implements Serializable {
+
+    private String code;
+
+    private String message;
+
+}
